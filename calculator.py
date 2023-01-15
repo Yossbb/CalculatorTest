4faa438542e751de586490759447a2ce4c268edf
Yosss <yocelin.vazquez@elektra.com.mx>
2023-01-15 01:42:36 -0600

    menu

--- calculator.py
+++ calculator.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 def calculator():
-    print("Hola mundo")
-
+    print("Menu de operaciones:")
+    print("1. Suma")
+    print("2. Resta")
+    print("3. Multiplicación")
+    print("4. División")
+    print("5. Raiz Cuadrada")
+    print("6. Exponente")
+    print("7. Seno")
+    print("8. Coseno")
+    print("9. Tangente\n")
+    print("0. Salir")
 calculator()
